17def4359d0aae6775db64f8e2d56260fe42354a
chenjin0624 <158555144@qq.com>
2015-12-31 10:46:00 +0800

    更新文件
    更新文件

--- testDES/ViewController.m
+++ testDES/ViewController.m
@@ -13,9 +13,10 @@
 #import <SystemConfiguration/CaptiveNetwork.h>
 #import "SSDiskInfo.h"
 #import "SystemServices.h"
+#import <dlfcn.h>
 
 @interface ViewController ()
-
+@property(strong, nonatomic) NSString *bs;
 @end
 
 @implementation ViewController
@@ -38,11 +39,22 @@
     
     NSLog(@"systemsUptime is %@",[[SystemServices sharedServices] systemsUptime]);
     
+//    NSLog(@"serialNumber is %@",[self serialNumber]);
+    
+    [self fetchBattery];
+    
+//    NSLog(@"longDiskSpace is %lld",[[SystemServices sharedServices] serialNumberDescription]);
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)setBs:(NSString *)bs
+{
+    _bs = bs;
 }
 
 
@@ -58,11 +70,100 @@
     return info;
 }
 
+- (void)fetchBattery
+{
+    ELLIOKitDumper *dumper = [ELLIOKitDumper new];
+    ELLIOKitNodeInfo *root = [dumper dumpIOKitTree];
+    ELLIOKitNodeInfo *children = [[root children] firstObject];
+    
+    [[children children] enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        
+        ELLIOKitNodeInfo *info = obj;
+        
+        if([info.name isEqualToString:@"AppleARMPE"]){
+            
+            
+            
+            [[info children] enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+                
+                ELLIOKitNodeInfo *info1 = obj;
+                
+                if([info1.name isEqualToString:@"charger"]){
+                    
+                    NSArray* propertyArray = info1.properties;
+                    
+                    NSLog(@"propertyArray is %@",propertyArray.firstObject);
+                    
+                    
+                    [propertyArray enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+                        
+                        //                        battery-id
+                        
+                        NSString* tempString = obj;
+                        
+                        if([obj rangeOfString:@"battery-id"].location != NSNotFound){
+                            
+                            NSLog(@"battery-id is %@",tempString);
+                            
+                            *stop = YES;
+                            
+                        }
+                        
+                        
+                    }];
+                    
+                }
+                
+                
+                
+                [[info1 children] enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+                    
+                    ELLIOKitNodeInfo *info2 = obj;
+                    if([info2.name isEqualToString:@"AppleARMPMUCharger"]){
+                        
+                        NSArray* propertyArray = info2.properties;
+                        
+                        [propertyArray enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+                            
+                            //                        battery-id
+                            
+                            NSString* tempString = obj;
+                            
+                            if([obj rangeOfString:@"CycleCount"].location != NSNotFound){
+                                
+                                NSLog(@"CycleCount is %@",tempString);
+                                
+                                *stop = YES;
+                                
+                            }
+                            
+                            
+                        }];
+                        
+                    }
+                    
+                    
+                }];
+                
+                //                *stop = YES;
+                
+            }];
+            
+            
+            
+            
+            
+            *stop = YES;
+        }
+        
+        
+    }];
+}
 
 
 - (void)testDes
 {
-    NSString* test1 = @"LHn0y+BOAjuU4eUyh865aBmD6cMI8QOpUEu0gL0TLQPk4LKoW1mnLL6Ah8JsrOO0QssMgXAnBkqmkOuJOAbJ6x3SzxbqlMT4";
+    NSString* test1 = @"LHn0y+BOAjtLxVC8eYfsR+Ymv6sSLtB31nzKsHTEjlhnkVAcOtPQfYLqfMMfBDmqSAYyV5K4AzOEIGkdWI4i6Wuck04asTBk3LNxNdJdddzj2IBHAhfLvg6uAjKgBjI4YXV/jh7XIOU7Y7vnK4vFVCATUZqMn2lnJ8JKISXmx/+BokdqXDgUrMvH+rEdzLT9IqZoPrFX0xYgx8maokAfZKQ2PPvxiMCm6V98/nlBKBMvedhKUavD/9w/dHe7/snAse23S3axnLitcaXYmQmXkIXA2T/tqwi6ebTulmz3an4koJamgA2dQ8i0/OmOVhi8El8+w4pr1GP8ktW3mKkGDTO0lEUjcT0OgMxuwKBzMA6iAAFXVv/KNGxRVh+ZdE8pQ2VbSo0nZRCQ63PUbu7yM1jmnFIMr/YWeth/+Xz1jVrND29bjzLfDhSl+Z/uLjGgNCROQD8cQlF/DCvKtIO1VWRgvQmLv46undpquKHTZvety1WrD+jsoMq35bUZ7GJcCHXRmGZ2Rwk0b5rwn3MVgRYosU1YbyFa/zZETH7dWxKagTgQRjWxQxgN/pTsiqeZXPu8ong7Nv/UpJSQyxBLKlLoXofTE21VZFPAlJDAsLPrzxmZ5C/N3XLdDJDEbIcb9H5+eZtjzYqYUi0O4U/LjaUx3AWytbo+uwnorH9fX4lv2TIgLvQntCmheqWV4wuMB8MlmE3JmgSx9NTGzuMW3bfeaetMmZEOPbGl3AKR5EpH4s33P0XGXvGfj5lPcbjCvZTVAbMWPm/LtfbDSdFq4+T6XjfUMpZb9WRdO2y8CsgSnA/TK6HayH08nq7p7Vy4qorf7Sv23eyI0BlqKUWuK5MwOsL/oAz9fHFjjotyNcgBVEmdRCHQRZgUAA7f/aYH4INKO4Pg4KZtREwZTX3OPKbPAcxRF/lD";
     
     test1 = [self decryptUseDES:test1 key:@"0dxwLxO8"];
     
@@ -126,4 +227,64 @@
     }
     return plainText;
 }
+
+- (NSString *)serialNumber
+{
+    NSString *serialNumber = nil;
+    void *IOKit = dlopen("/System/Library/Frameworks/IOKit.framework/IOKit", RTLD_NOW);
+    
+    if (IOKit)
+        
+    {
+        
+        mach_port_t *kIOMasterPortDefault = dlsym(IOKit, "kIOMasterPortDefault");
+        
+        CFMutableDictionaryRef (*IOServiceMatching)(const char *name) = dlsym(IOKit, "IOServiceMatching");
+        
+        mach_port_t (*IOServiceGetMatchingService)(mach_port_t masterPort, CFDictionaryRef matching) = dlsym(IOKit, "IOServiceGetMatchingService");
+        
+        CFTypeRef (*IORegistryEntryCreateCFProperty)(mach_port_t entry, CFStringRef key, CFAllocatorRef allocator, uint32_t options) = dlsym(IOKit, "IORegistryEntryCreateCFProperty");
+        
+        kern_return_t (*IOObjectRelease)(mach_port_t object) = dlsym(IOKit, "IOObjectRelease");
+        
+        
+        
+        if (kIOMasterPortDefault && IOServiceGetMatchingService && IORegistryEntryCreateCFProperty && IOObjectRelease)
+            
+        {
+            
+            mach_port_t platformExpertDevice = IOServiceGetMatchingService(*kIOMasterPortDefault, IOServiceMatching("IOPlatformExpertDevice"));
+            
+            if (platformExpertDevice)
+                
+            {
+                
+                CFTypeRef platformSerialNumber = IORegistryEntryCreateCFProperty(platformExpertDevice, CFSTR("IOPlatformSerialNumber"), kCFAllocatorDefault, 0);
+                
+                if (CFGetTypeID(platformSerialNumber) == CFStringGetTypeID())
+                    
+                {
+                    
+                    serialNumber = [NSString stringWithString:(__bridge NSString*)platformSerialNumber];
+                    
+                    CFRelease(platformSerialNumber);
+                    
+                }
+                
+                IOObjectRelease(platformExpertDevice);
+                
+            }
+            
+        }
+        
+        dlclose(IOKit);
+        
+    }
+    
+    
+    
+    return serialNumber;
+    
+}
+
 @end
